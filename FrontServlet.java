4926c0010e790a7f03e612868c86edbfc7b1b898
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2023-03-07 18:05:14 +0300

    Add files via upload

---
+++ FrontServlet.java
@@ -0,0 +1,34 @@
+package etu2007.framework.servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet{
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+        PrintWriter out = resp.getWriter();
+        String requete = req.getRequestURI();
+        String valeur = req.getQueryString();
+        String[] info = requete.split("/"); 
+        out.println("Page: " + info[2]);
+        out.println("Les donnees sont:");
+        String[] valiny = valeur.split("&&");
+        for (String rep : valiny) {
+           out.println(rep); 
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
